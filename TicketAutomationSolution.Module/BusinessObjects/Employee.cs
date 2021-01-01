af5d06572663ab663fe4f95032c554e58e1dff1b
Geek911 <moseschawawa@gmail.com>
2021-01-02 00:08:55 +0200

    Added An Employee Table for refined Adding of Employees

---
+++ TicketAutomationSolution.Module/BusinessObjects/Employee.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using System;
+using System.Linq;
+
+namespace TicketAutomationSolution.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    public class Employee : User
+    {
+        public Employee(Session session) : base(session)
+        { }
+
+
+    }
+}
